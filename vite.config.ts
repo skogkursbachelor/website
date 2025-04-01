7ed5aab9ba863b9ad25f7e3d11b645025bc97f22
Erik Bjørnsen <bjornsen.erik@gmail.com>
2025-04-01 14:45:30 +0200

    actions: renamed workflow, removed internal proxies

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://gts.nve.no",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      "/skogsbilveg": {
-        target: "http://localhost:8080/api/v1/forestryroads",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/skogsbilveg/, ""),
-      },
-    },
-  },
+  server: {},
 });
