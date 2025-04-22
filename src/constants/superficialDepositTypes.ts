065ddf0b1f0cdd3b77546e8d785a888ae4ec99b1
Simon <106014761+simonhoumb@users.noreply.github.com>
2025-04-22 23:26:09 +0200

    feat: added superficial deposit type list

---
+++ src/constants/superficialDepositTypes.ts
@@ -0,0 +1,390 @@
+export const superficialDepositTypes = [
+  {
+    code: 1,
+    name: "Løsmasser/berggrunn under vann,uspesifisert",
+  },
+  {
+    code: 10,
+    name: "Morenemateriale, uspesifisert",
+  },
+  {
+    code: 11,
+    name: "Morenemateriale, sammenhengende dekke, stedvis med stor mektighet",
+  },
+  {
+    code: 12,
+    name: "Morenemateriale, usammenhengende eller tynt dekke over berggrunnen",
+  },
+  {
+    code: 13,
+    name: "Moreneleire",
+  },
+  {
+    code: 14,
+    name: "Avsmeltningsmorene (Ablasjonsmorene)",
+  },
+  {
+    code: 15,
+    name: "Randmorene/randmorenesone",
+  },
+  {
+    code: 16,
+    name: "Drumlin",
+  },
+  {
+    code: 17,
+    name: "Rogenmorene",
+  },
+  {
+    code: 20,
+    name: "Breelvavsetning (Glasifluvial avsetning)",
+  },
+  {
+    code: 21,
+    name: "Breelv- og elveavsetning",
+  },
+  {
+    code: 22,
+    name: "Ryggformet breelvavsetning (Esker)",
+  },
+  {
+    code: 23,
+    name: "Haugformet breelvavsetning (Kame)",
+  },
+  {
+    code: 30,
+    name: "Bresjø- eller brekammeravsetning (Glasilakustrin avsetning)",
+  },
+  {
+    code: 31,
+    name: "Breelv- og bresjø-/brekammeravsetning (Glasifluvial og glasilakustrin avsetning)",
+  },
+  {
+    code: 35,
+    name: "Innsjøavsetning (Lakustrin avsetning)",
+  },
+  {
+    code: 36,
+    name: "Bresjø-/brekammer og innsjøavsetning (Glasilakustrin og lakustrin avsetning)",
+  },
+  {
+    code: 37,
+    name: "Strandavsetning innsjø og/eller bresjø",
+  },
+  {
+    code: 40,
+    name: "Hav- og fjordavsetning, uspesifisert",
+  },
+  {
+    code: 41,
+    name: "Hav- og fjordavsetning, sammenhengende dekke, stedvis med stor mektighet",
+  },
+  {
+    code: 42,
+    name: "Marin strandavsetning, sammenhengende dekke",
+  },
+  {
+    code: 43,
+    name: "Hav-, fjord- og strandavsetning, usammenhengende eller tynt dekke over berggrunnen",
+  },
+  {
+    code: 44,
+    name: "Skjellsand (maringeologi)",
+  },
+  {
+    code: 45,
+    name: "Marin gytje",
+  },
+  {
+    code: 50,
+    name: "Elve- og bekkeavsetning (Fluvial avsetning)",
+  },
+  {
+    code: 51,
+    name: "Elveavsetning, sammenhengende dekke",
+  },
+  {
+    code: 52,
+    name: "Elve- og bekkeavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 53,
+    name: "Flomavsetning bresjøtapning (uspesifisert)",
+  },
+  {
+    code: 54,
+    name: "Flomavsetning fra bresjøtapping, sammenhengende",
+  },
+  {
+    code: 55,
+    name: "Flomavsetning fra bresjøtapping, usammenhengende eller tynt dekke over berggrunnen",
+  },
+  {
+    code: 56,
+    name: "Flomavsetning",
+  },
+  {
+    code: 57,
+    name: "Flomavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 60,
+    name: "Vindavsetning (Eolisk avsetning)",
+  },
+  {
+    code: 70,
+    name: "Forvitringsmateriale, ikke inndelt etter mektighet",
+  },
+  {
+    code: 71,
+    name: "Forvitringsmateriale",
+  },
+  {
+    code: 72,
+    name: "Forvitringsmateriale, usammenhengende eller tynt dekke over berggrunnen",
+  },
+  {
+    code: 73,
+    name: "Forvitringsmateriale, stein- og blokkrikt (blokkhav)",
+  },
+  {
+    code: 80,
+    name: "Skredmateriale, ikke inndelt etter mektighet",
+  },
+  {
+    code: 81,
+    name: "Skredmateriale, sammenhengende dekke",
+  },
+  {
+    code: 82,
+    name: "Skredmateriale, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 88,
+    name: "Steinbreavsetning",
+  },
+  {
+    code: 90,
+    name: "Torv og myr",
+  },
+  {
+    code: 100,
+    name: "Tynt dekke av organisk materiale over berggrunn",
+  },
+  {
+    code: 101,
+    name: "Usammenhengende eller tynt løsmassedekke over berggrunnen, flere løsmassetyper, uspesifisert",
+  },
+  {
+    code: 102,
+    name: "Sammenhengende løsmassedekke av flere jordarter",
+  },
+  {
+    code: 110,
+    name: "Bart fjell/fjell med tynt torvdekke, uspesifisert",
+  },
+  {
+    code: 120,
+    name: "Fyllmasse (antropogent materiale)",
+  },
+  {
+    code: 121,
+    name: "Steintipp ",
+  },
+  {
+    code: 122,
+    name: "Menneskepåvirket materiale, ikke nærmere spesifisert",
+  },
+  {
+    code: 130,
+    name: "Bart fjell",
+  },
+  {
+    code: 140,
+    name: "Bart fjell/fjell med usammenhengende eller tynt løsmassedekke",
+  },
+  {
+    code: 150,
+    name: "Harde sedimenter eller sedimentære bergarter (maringeologi)",
+  },
+  {
+    code: 200,
+    name: "Marin suspensjonsavsetning (maringeologi)",
+  },
+  {
+    code: 201,
+    name: "Marin bunnstrømavsetning (maringeologi)",
+  },
+  {
+    code: 202,
+    name: "Glasimarin avsetning (maringeologi)",
+  },
+  {
+    code: 203,
+    name: "Iskontaktavsetning (maringeologi)",
+  },
+  {
+    code: 204,
+    name: "Utvaskingslag (maringeologi)",
+  },
+  {
+    code: 205,
+    name: "Glasifluvial deltaavsetning (maringeologi)",
+  },
+  {
+    code: 206,
+    name: "Fluvial deltaavsetning (maringeologi)",
+  },
+  {
+    code: 207,
+    name: "Tidevannsavsetning (maringeologi)",
+  },
+  {
+    code: 208,
+    name: "Estuarin avsetning (maringeologi)",
+  },
+  {
+    code: 209,
+    name: "Levé avsetning (maringeologi)",
+  },
+  {
+    code: 210,
+    name: "Grunnmarin avsetning (maringeologi)",
+  },
+  {
+    code: 211,
+    name: "Konturittavsetning (maringeologi)",
+  },
+  {
+    code: 212,
+    name: "Turbitittavsetning (maringeologi)",
+  },
+  {
+    code: 213,
+    name: "Debrisstrømavsetning (maringeologi)",
+  },
+  {
+    code: 214,
+    name: "Undersjøisk vifteavsetning (maringeologi)",
+  },
+  {
+    code: 215,
+    name: "Kanalsavsetning (maringeologi)",
+  },
+  {
+    code: 216,
+    name: "Dypmarin avsetning (maringeologi)",
+  },
+  {
+    code: 217,
+    name: "Bioklastisk avsetning (maringeologi)",
+  },
+  {
+    code: 218,
+    name: "Vulkanosedimentær avsetning (maringeologi)",
+  },
+  {
+    code: 219,
+    name: "Lagdelte sedimenter (>1 m) over debrisstrøm (maringeologi)",
+  },
+  {
+    code: 220,
+    name: "Karbonatskorpe (maringeologi)",
+  },
+  {
+    code: 240,
+    name: "Skredmateriale, dekket av yngre sedimenter (maringeologi)",
+  },
+  {
+    code: 241,
+    name: "Skredmateriale, delvis dekket av yngre sedimenter (maringeologi)",
+  },
+  {
+    code: 242,
+    name: "Skredmateriale og hemipelagiske avsetninger (maringeologi)",
+  },
+  {
+    code: 250,
+    name: "Uspesifisert marin avsetning (maringeologi)",
+  },
+  {
+    code: 301,
+    name: "Jord- og flomskredavsetning",
+  },
+  {
+    code: 302,
+    name: "Jord- og flomskredavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 303,
+    name: "Leirskredavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 304,
+    name: "Leirskredavsetning, usammenhengende eller tynt dekke over berggrunnen",
+  },
+  {
+    code: 305,
+    name: "Fjellskredavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 306,
+    name: "Fjellskredavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 307,
+    name: "Steinsprangavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 308,
+    name: "Steinsprangavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 309,
+    name: "Snøskredavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 310,
+    name: "Snøskredavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 311,
+    name: "Steinskredavsetning, stedvis med stor metighet",
+  },
+  {
+    code: 312,
+    name: "Steinskredavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 313,
+    name: "Snø- og jordskredavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 314,
+    name: "Snø- og jordskredavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 315,
+    name: "Jordskred- og steinsprangavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 316,
+    name: "Jordskred- og steinsprangavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 317,
+    name: "Snø- og steinsprangavsetning, stedvis med stor mektighet",
+  },
+  {
+    code: 318,
+    name: "Snø- og steinsprangavsetning, usammenhengende eller tynt dekke",
+  },
+  {
+    code: 320,
+    name: "Sigejord med høyt organisk innhold",
+  },
+  {
+    code: 321,
+    name: "Steinrikt, sigende skråningsmateriale",
+  },
+];
