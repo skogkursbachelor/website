2e67110eae8c8ddc9d8d79f1ab2bb2edb7862472
Erik Bjørnsen <bjornsen.erik@gmail.com>
2025-04-28 14:54:50 +0200

    unfeat: removed copernicus layer

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -25,154 +25,164 @@ import FrostDepthLayer from "./layers/FrostDepthLayer.tsx";
 import SuperficialDepositsLayer from "./layers/SuperficialDepositsLayer.tsx";
 import NibioSoilMoistureLayer from "./layers/NibioSoilMoistureLayer.tsx";
 import ForestryRoadLayer, {
-  setHoveredFeature,
+	setHoveredFeature,
 } from "./layers/ForestryRoadLayer.tsx";
-import CopernicusSoilMoistureLayer from "./layers/CopernicusSoilMoistureLayer.tsx";
 import SoilSaturationLayer from "./layers/SoilSaturationLayer.tsx";
 import { Coordinate } from "ol/coordinate";
 
 const MapContainer: React.FC = () => {
-  const mapRef = useRef<HTMLDivElement>(null);
+	const mapRef = useRef<HTMLDivElement>(null);
 
-  const [mapInstance, setMapInstance] = useState<OpenLayersMap | null>(null);
-  const [date, setDate] = useState(new Date());
-  const [isLayerSidebarOpen, setIsLayerSidebarOpen] = useState(false);
-  const [isLegendSidebarOpen, setIsLegendSidebarOpen] = useState(false);
-  const [waterSaturationThresholdsState, setWaterSaturationThresholdsState] =
-    useState<Map<number, { min: number; max: number }>>(() =>
-      createDefaultThresholds()
-    );
-  const [frostDepthThresholdState, setFrostDepthThresholdState] =
-    useState<number>(10);
-  const [clickCoord, setClickCoord] = useState<Coordinate | null>(null);
-  const [clickedFeature, setClickedFeature] = useState<FeatureLike | null>(
-    null
-  );
+	const [mapInstance, setMapInstance] = useState<OpenLayersMap | null>(null);
+	const [date, setDate] = useState(new Date());
+	const [isLayerSidebarOpen, setIsLayerSidebarOpen] = useState(false);
+	const [isLegendSidebarOpen, setIsLegendSidebarOpen] = useState(false);
+	const [waterSaturationThresholdsState, setWaterSaturationThresholdsState] =
+		useState<Map<number, { min: number; max: number }>>(() =>
+			createDefaultThresholds()
+		);
+	const [frostDepthThresholdState, setFrostDepthThresholdState] =
+		useState<number>(10);
+	const [clickCoord, setClickCoord] = useState<Coordinate | null>(null);
+	const [clickedFeature, setClickedFeature] = useState<FeatureLike | null>(
+		null
+	);
 
-  // Define layers
-  const layers = [
-    SuperficialDepositsLayer,
-    FrostDepthLayer,
-    SoilSaturationLayer,
-    NibioSoilMoistureLayer,
-    ForestryRoadLayer,
-    CopernicusSoilMoistureLayer,
-  ];
+	// Define layers
+	const layers = [
+		SuperficialDepositsLayer,
+		FrostDepthLayer,
+		SoilSaturationLayer,
+		NibioSoilMoistureLayer,
+		ForestryRoadLayer,
+	];
 
-  function createDefaultThresholds(): Map<
-    number,
-    { min: number; max: number }
-  > {
-    const map = new Map<number, { min: number; max: number }>();
-    superficialDepositTypes.forEach((type) => {
-      // Set default values for min and max
-      map.set(type.code, { min: 45, max: 75 });
-    });
-    return map;
-  }
+	function createDefaultThresholds(): Map<
+		number,
+		{ min: number; max: number }
+	> {
+		const map = new Map<number, { min: number; max: number }>();
+		superficialDepositTypes.forEach((type) => {
+			// Set default values for min and max
+			map.set(type.code, { min: 45, max: 75 });
+		});
+		return map;
+	}
 
-  useEffect(() => {
-    setWaterSaturationThresholds(waterSaturationThresholdsState);
-  }, [waterSaturationThresholdsState]);
+	useEffect(() => {
+		setWaterSaturationThresholds(waterSaturationThresholdsState);
+	}, [waterSaturationThresholdsState]);
 
-  useEffect(() => {
-    setFrostDepthThreshold(frostDepthThresholdState);
-  }, [frostDepthThresholdState]);
+	useEffect(() => {
+		setFrostDepthThreshold(frostDepthThresholdState);
+	}, [frostDepthThresholdState]);
 
-  // Initialize the map
-  useLayoutEffect(() => {
-    if (!mapRef.current) return;
+	// Initialize the map
+	useLayoutEffect(() => {
+		if (!mapRef.current) return;
 
-    const attribution = new Attribution({
-      collapsible: false,
-    });
+		const attribution = new Attribution({
+			collapsible: false,
+		});
 
-    const map = new OpenLayersMap({
-      target: mapRef.current,
-      view: new View({
-        center: fromLonLat([10, 59]),
-        zoom: 5,
-      }),
-      controls: [attribution],
-      layers: layers,
-    });
+		const map = new OpenLayersMap({
+			target: mapRef.current,
+			view: new View({
+				center: fromLonLat([10, 59]),
+				zoom: 5,
+			}),
+			controls: [attribution],
+			layers: layers,
+		});
 
-    setMapInstance(map);
+		setMapInstance(map);
 
-    // Pointer move event for hover effect
-    map.on("pointermove", (event) => {
-      const feature = map.forEachFeatureAtPixel(event.pixel, (feat) => feat, {
-        hitTolerance: 10,
-      });
-      setHoveredFeature(feature || null);
-    });
+		// Pointer move event for hover effect
+		map.on("pointermove", (event) => {
+			const feature = map.forEachFeatureAtPixel(
+				event.pixel,
+				(feat) => feat,
+				{
+					hitTolerance: 10,
+				}
+			);
+			setHoveredFeature(feature || null);
+		});
 
-    map.on("singleclick", (event) => {
-      const feature = map.forEachFeatureAtPixel(event.pixel, (feat) => feat, {
-        hitTolerance: 5,
-      });
+		map.on("singleclick", (event) => {
+			const feature = map.forEachFeatureAtPixel(
+				event.pixel,
+				(feat) => feat,
+				{
+					hitTolerance: 5,
+				}
+			);
 
-      if (feature && feature.get("layerId") === "forestryRoad") {
-        setClickCoord(event.coordinate);
-        setClickedFeature(feature);
-      } else {
-        setClickCoord(null);
-        setClickedFeature(null);
-      }
-    });
+			if (feature && feature.get("layerId") === "forestryRoad") {
+				setClickCoord(event.coordinate);
+				setClickedFeature(feature);
+			} else {
+				setClickCoord(null);
+				setClickedFeature(null);
+			}
+		});
 
-    return () => map.setTarget(undefined);
-  }, []); // Empty dependency array ensures it runs once when the component mounts
+		return () => map.setTarget(undefined);
+	}, []); // Empty dependency array ensures it runs once when the component mounts
 
-  return (
-    <div className="map-wrapper">
-      <div className="date-picker-container">
-        <DatePicker date={date} setDate={setDate} layers={layers} />
-      </div>
+	return (
+		<div className="map-wrapper">
+			<div className="date-picker-container">
+				<DatePicker date={date} setDate={setDate} layers={layers} />
+			</div>
 
-      <div ref={mapRef} className="map-container">
-        {mapInstance && (
-          <>
-            <BaseLayerGroup map={mapInstance} />
-            <MapZoom map={mapInstance} />
-            <MapOverview map={mapInstance} />
-            <MapScaleLine map={mapInstance} />
-            <MapGeolocation map={mapInstance} />
-            <BaseLayerSelector map={mapInstance} />
-            <SidebarThresholdConfig
-              waterSaturationThresholds={waterSaturationThresholdsState}
-              setWaterSaturationThresholdsState={
-                setWaterSaturationThresholdsState
-              }
-              setFrostDepthThresholdState={setFrostDepthThresholdState}
-              isLayerSidebarOpen={isLayerSidebarOpen}
-              isLegendSidebarOpen={isLegendSidebarOpen}
-            />
-            <SidebarLayerSelector
-              map={mapInstance}
-              layers={layers}
-              setLayerSidebarOpen={setIsLayerSidebarOpen}
-            />
-            <SidebarLegendOverview
-              map={mapInstance}
-              layers={layers}
-              isLayerSidebarOpen={isLayerSidebarOpen}
-              setLegendSidebarOpen={setIsLegendSidebarOpen}
-            />
-            <QueryOverlay
-              map={mapInstance}
-              coordinate={clickCoord}
-              feature={clickedFeature}
-              onClose={() => {
-                setClickCoord(null);
-                setClickedFeature(null);
-              }}
-            />
-          </>
-        )}
-      </div>
-    </div>
-  );
+			<div ref={mapRef} className="map-container">
+				{mapInstance && (
+					<>
+						<BaseLayerGroup map={mapInstance} />
+						<MapZoom map={mapInstance} />
+						<MapOverview map={mapInstance} />
+						<MapScaleLine map={mapInstance} />
+						<MapGeolocation map={mapInstance} />
+						<BaseLayerSelector map={mapInstance} />
+						<SidebarThresholdConfig
+							waterSaturationThresholds={
+								waterSaturationThresholdsState
+							}
+							setWaterSaturationThresholdsState={
+								setWaterSaturationThresholdsState
+							}
+							setFrostDepthThresholdState={
+								setFrostDepthThresholdState
+							}
+							isLayerSidebarOpen={isLayerSidebarOpen}
+							isLegendSidebarOpen={isLegendSidebarOpen}
+						/>
+						<SidebarLayerSelector
+							map={mapInstance}
+							layers={layers}
+							setLayerSidebarOpen={setIsLayerSidebarOpen}
+						/>
+						<SidebarLegendOverview
+							map={mapInstance}
+							layers={layers}
+							isLayerSidebarOpen={isLayerSidebarOpen}
+							setLegendSidebarOpen={setIsLegendSidebarOpen}
+						/>
+						<QueryOverlay
+							map={mapInstance}
+							coordinate={clickCoord}
+							feature={clickedFeature}
+							onClose={() => {
+								setClickCoord(null);
+								setClickedFeature(null);
+							}}
+						/>
+					</>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default MapContainer;
